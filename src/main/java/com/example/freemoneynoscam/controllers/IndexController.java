e0c9e2612950e3c8e50863b257c3997b73dd380a
nicklasdean <61466422+nicklasdean@users.noreply.github.com>
2021-10-19 13:42:39 +0200

    first

--- src/main/java/com/example/freemoneynoscam/controllers/IndexController.java
+++ src/main/java/com/example/freemoneynoscam/controllers/IndexController.java
@@ -2,6 +2,8 @@ package com.example.freemoneynoscam.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.context.request.WebRequest;
 
 @Controller
 public class IndexController {
@@ -9,4 +11,12 @@ public class IndexController {
     public String index(){
         return "index";
     }
+
+    @PostMapping("/test")
+    public String userEmail(WebRequest wr){
+        String parameter = wr.getParameter("test-input");
+        System.out.println(parameter);
+        return "index";
+    }
+
 }
