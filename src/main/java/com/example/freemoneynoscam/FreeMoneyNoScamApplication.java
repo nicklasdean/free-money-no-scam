bcfca090e92df84a371dbc15d83a785fa75dd060
nicklasdean <61466422+nicklasdean@users.noreply.github.com>
2021-10-19 09:14:53 +0200

    first

---
+++ src/main/java/com/example/freemoneynoscam/FreeMoneyNoScamApplication.java
@@ -0,0 +1,13 @@
+package com.example.freemoneynoscam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeMoneyNoScamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreeMoneyNoScamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/freemoneynoscam/FreeMoneyNoScamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.freemoneynoscam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeMoneyNoScamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
