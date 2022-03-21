fa58515b973725604ec5921cc34665bbd09acd69
nicklasdean <61466422+nicklasdean@users.noreply.github.com>
2022-03-21 10:58:19 +0100

    ready or not

--- src/main/java/com/example/freemoneynoscam/controllers/IndexController.java
+++ src/main/java/com/example/freemoneynoscam/controllers/IndexController.java
@@ -3,6 +3,8 @@ package com.example.freemoneynoscam.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.WebRequest;
 
 @Controller
@@ -10,5 +12,15 @@ public class IndexController {
     @GetMapping("/")
     public String index(){
         return "index";
+    }
+
+    @PostMapping("/test")
+    public String test(WebRequest dataFromForm){
+        System.out.println(dataFromForm.getParameter("email"));
+        System.out.println(dataFromForm.getParameter("email"));
+        System.out.println(dataFromForm.getParameter("email"));
+        System.out.println(dataFromForm.getParameter("email"));
+        System.out.println(dataFromForm.getParameter("email"));
+        return "redirect:/";
     }
 }
